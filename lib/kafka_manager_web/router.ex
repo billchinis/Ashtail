123f280aec3d060cd82b59d5be758c9b8db1b1f4
Bill Chinis <bill.chinis@gmail.com>
2026-09-10 03:06:08 +0300

    feat(produce): produce form for a single message

--- lib/kafka_manager_web/router.ex
+++ lib/kafka_manager_web/router.ex
@@ -19,6 +19,7 @@ defmodule KafkaManagerWeb.Router do
 
     live "/", TopicLive.Index, :index
     live "/topics/:topic", TopicLive.Show, :show
+    live "/topics/:topic/produce", TopicLive.Produce, :produce
     live "/topics/:topic/partitions/:partition", MessageLive.Index, :index
   end
 
